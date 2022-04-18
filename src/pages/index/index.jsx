1c97829f67b0e72c4fa71ab7d0da26a045790820
Elton Zheng <elton.zheng.sh@gmail.com>
2022-04-18 14:56:10 +0800

    Initialize the project.

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from '@tarojs/components';
+
+const Index = () => {
+  return (
+    <View>
+      <Text className='text-3xl text-amber-700 m-4'>Hello world!</Text>
+    </View>
+  );
+};
+
+export default Index;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // v3 版本的 tailwindcss 有些不同
+  corePlugins: {
+    preflight: false,
+  },
+};
